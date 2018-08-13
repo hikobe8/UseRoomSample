ff1b5efb4f9a4d1b28a1a0077d75031965e2eb5f
hikobe8 <hikobe8@163.com>
2018-08-13 22:53:47 +0800

    add WordRoomDatabase

---
+++ app/src/main/java/com/ray/useroomsample/entity/WordRoomDatabase.java
@@ -0,0 +1,34 @@
+package com.ray.useroomsample.entity;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.ray.useroomsample.dao.WordDao;
+
+/**
+ * Author : hikobe8@github.com
+ * Time : 2018/8/13 下午10:49
+ * Description :
+ */
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+    public abstract WordDao wordDao();
+
+    private static WordRoomDatabase sInstance;
+
+    public static WordRoomDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database").build();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+}
